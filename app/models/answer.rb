fe73fb1cd3d2c41fec865e015adca798dfda0184
中島正矩 <nakaji-m@nakajimamasanorinoMacBook-Pro.local>
2025-06-14 23:50:49 +0900

    modelの修正

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,16 @@
 class Answer < ApplicationRecord
+  self.primary_key = 'id'
+  
   belongs_to :course
   belongs_to :question
   
   validates :form_token, :content, presence: true
+  
+  before_create :generate_uuid
+  
+  private
+  
+  def generate_uuid
+    self.id = SecureRandom.uuid if id.blank?
+  end
 end
--- app/models/option.rb
+++ app/models/option.rb
@@ -1,5 +1,15 @@
 class Option < ApplicationRecord
+  self.primary_key = 'id'
+  
   belongs_to :question
   
   validates :text_ja, :text_en, :order, :filter_type, presence: true
+  
+  before_create :generate_uuid
+  
+  private
+  
+  def generate_uuid
+    self.id = SecureRandom.uuid if id.blank?
+  end
 end
