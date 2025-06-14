18e6e8bd6f47ff39432785d065b250eef4b433d1
中島正矩 <nakaji-m@nakajimamasanorinoMacBook-Pro.local>
2025-06-15 02:35:01 +0900

    pumaの設定変更

--- config/puma.rb
+++ config/puma.rb
@@ -23,8 +23,13 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT", 3000)
+# Bind to all interfaces in production, localhost in development
+if ENV["RAILS_ENV"] == "production"
+  bind "tcp://0.0.0.0:#{ENV.fetch('PORT', 3000)}"
+else
+  # In development, bind to localhost
+  bind "tcp://127.0.0.1:#{ENV.fetch('PORT', 3000)}"
+end
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
