e1e3cc1c2035edbf663b3784427f2d9b8000bae2
中島正矩 <nakaji-m@nakajimamasanorinoMacBook-Pro.local>
2025-06-14 19:29:12 +0900

    teacherを削除

--- db/migrate/20250614084419_create_teachers.rb
+++
@@ -1,8 +0,0 @@
-class CreateTeachers < ActiveRecord::Migration[7.2]
-  def change
-    create_table :teachers, id: :string, limit: 36 do |t|
-      t.string :name, limit: 255, null: false
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20250614084443_create_course_teachers.rb
+++ db/migrate/20250614084443_create_course_teachers.rb
@@ -2,14 +2,11 @@ class CreateCourseTeachers < ActiveRecord::Migration[7.2]
   def change
     create_table :course_teachers, id: false do |t|
       t.string :course_id, limit: 36, null: false
-      t.string :teacher_id, limit: 36, null: false
+      t.string :name, limit: 255, null: false
       t.timestamps
     end
 
-    add_index :course_teachers, [:course_id, :teacher_id], unique: true
     add_index :course_teachers, :course_id, name: 'idx_ct_course'
-    add_index :course_teachers, :teacher_id, name: 'idx_ct_teacher'
     add_foreign_key :course_teachers, :courses, column: :course_id, name: 'fk_ct_course'
-    add_foreign_key :course_teachers, :teachers, column: :teacher_id, name: 'fk_ct_teacher'
   end
 end
