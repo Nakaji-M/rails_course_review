2c9bbff6fb695587f3babbd1605782ca6a9a6e9f
中島正矩 <nakaji-m@nakajimamasanorinoMacBook-Pro.local>
2025-06-15 23:45:20 +0900

    一部のindexを削除

--- db/migrate/20250614084439_create_course_departments.rb
+++ db/migrate/20250614084439_create_course_departments.rb
@@ -2,7 +2,7 @@ class CreateCourseDepartments < ActiveRecord::Migration[7.2]
   def change
     create_table :course_departments, id: false do |t|
       t.references :course, type: :string, limit: 36, null: false, foreign_key: true, index: { name: 'idx_cd_course' }
-      t.references :department, type: :string, limit: 36, null: false, foreign_key: true, index: { name: 'idx_cd_department' }
+      t.references :department, type: :string, limit: 36, null: false, foreign_key: true, index: false
       t.timestamps
     end
 
--- db/migrate/20250614084448_create_answers.rb
+++ db/migrate/20250614084448_create_answers.rb
@@ -3,7 +3,7 @@ class CreateAnswers < ActiveRecord::Migration[7.2]
     create_table :answers, id: :string, limit: 36 do |t|
       t.string :form_token, limit: 255, null: false
       t.references :course, type: :string, limit: 36, null: false, foreign_key: true, index: { name: 'idx_answer_course' }
-      t.references :question, type: :string, limit: 36, null: false, foreign_key: true, index: { name: 'idx_answer_question' }
+      t.references :question, type: :string, limit: 36, null: false, foreign_key: true, index: false
       t.text :content, null: false
       t.timestamps
     end
